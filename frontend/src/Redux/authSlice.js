2e01e2cb6019700f15e55fe9ac7394c729b5c49f
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-07-27 09:21:28 +0530

    Update authSlice.js

--- frontend/src/Redux/authSlice.js
+++ frontend/src/Redux/authSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk,createSlice } from "@reduxjs/toolkit";
 import {toast} from "react-hot-toast"
 import axios from "axios";
-import { resolveInclude } from "ejs";
+
 
 
 const initialState = {
