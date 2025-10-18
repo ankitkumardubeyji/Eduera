dd370f486663e6ee39967544e789893e8b3406e1
ANKIT KUMAR DUBEY <122801994+ankitkumardubeyji@users.noreply.github.com>
2025-10-18 18:17:39 +0530

    Update payment.routes.js

--- backend/routes/payment.routes.js
+++ backend/routes/payment.routes.js
@@ -1,18 +1,21 @@
 import { Router } from "express";
 import { authorisedRoles, verifyJWT } from "../src/middlewares/auth.middleware.js";
-import { allPayments, buySubscription, cancelSubscription, getRazorpayKey, verifySubscription } from "../src/controllers/payment.controller.js";
+import { 
+  allPayments, 
+  buySubscription, 
+  cancelSubscription, 
+  getRazorpayKey, 
+  verifySubscription 
+} from "../src/controllers/payment.controller.js";
 
-const router = Router()
+const router = Router();
 
+router.use(verifyJWT);
 
-router.use(verifyJWT)
+router.route("/razorpay-key").get(getRazorpayKey);
+router.route("/subscribe").post(buySubscription);
+router.route("/verify").post(verifySubscription);
+router.post("/unsubscribe", cancelSubscription);
+router.get("/", authorisedRoles("ADMIN"), allPayments);
 
-router.route("/razorpay-key").get(getRazorpayKey)
-router.route("/subscribe").post(buySubscription)
-router.route("/verify").post(verifySubscription)
-router.post("/unsubscribe").post(cancelSubscription)
-router.post("/").get(authorisedRoles('ADMIN'),allPayments)
-
-
-
-export default router
+export default router;
